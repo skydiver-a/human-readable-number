9c6d535154287e95841b5222f3969877d012c8f3
Anna Kravchenko <flamedance.ann@gmail.com>
2022-01-07 08:10:06 +0300

    feat: add task's solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    let arrUnits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrDozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number < 20) {
+        return arrUnits[number];
+    } else if (number < 100) {
+        return (number % 10 === 0) ? arrDozens[(~~(number / 10)) - 2] : arrDozens[(~~(number / 10)) - 2] + ' ' + toReadable(number % 10);
+    } else if (number < 1000){
+        return (number % 100 === 0) ? arrUnits[~~(number / 100)] + ' hundred' : arrUnits[~~(number / 100)] + ' hundred ' + toReadable(number % 100);
+    };
 }
